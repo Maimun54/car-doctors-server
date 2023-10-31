f75ca2a1f6eed05a8f8dda9f2af4b39868f3c141
Maimun54 <maimunislam@gmail.com>
2023-10-31 09:04:35 +0600

    token added with jwt

---
+++ __jwt__steps_.js
@@ -0,0 +1,25 @@
+/*
+*install jsonwebtoken
+*jwt.sign(payload,secret,{expiresIn:'1hr'})
+*token client
+*/
+
+
+
+/*
+*how to store token in server side
+1.memory -->ok type
+2.local storage -->ok type (xss)
+3. cookies: http only
+*/
+/*
+* set cookies with http only for development secure:false
+* cors
+ app.use(cors({
+  
+  origin:['http://localhost:5173'],
+  credentials:true
+  
+}))
+3.client side axios 
+*/
